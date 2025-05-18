ba6e8334db5fdadd96b711faa4bf14e4ae065e3c
GppCalcagno <gpp.calcagno@gmail.com>
2025-05-18 16:38:12 +0200

    change logo

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 export default function Logo() {
     return (
-        <Link href="/" className="flex font-bold items-center hover:underline ">
-        <Image src="/logo.png" alt="Logo" width={33} height={33} />
-        <span className="ml-1 text-xl">Engineered Log </span>
+        <Link href="/" className="flex font-semibold items-center hover:underline underline-offset-4 ">
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <span className="ml-1 text-lg">Engineered Log </span>
       </Link>     
     );
   }
